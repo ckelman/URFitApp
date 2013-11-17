2383ff10388625dcaedc893d8837e6aca57aece9
ckelman <cikelman@u.rochester.edu>
2013-11-17 18:39:48 -0500

    File formatting

--- URFitProject/URFit/src/main/java/com/URFitness/HomeActivity.java
+++ URFitProject/URFit/src/main/java/com/URFitness/HomeActivity.java
@@ -11,12 +11,12 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
 
-public class MainActivity extends ActionBarActivity {
+public class HomeActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_home);
 
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
@@ -30,7 +30,7 @@ public class MainActivity extends ActionBarActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+        getMenuInflater().inflate(R.menu.home, menu);
         return true;
     }
 
@@ -57,12 +57,14 @@ public class MainActivity extends ActionBarActivity {
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            View rootView = inflater.inflate(R.layout.fragment_home, container, false);
             return rootView;
         }
     }
 
+    public void goWeights(View view)
+    {
 
-
+    }
 
 }
