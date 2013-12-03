21cf0b08004645d5314320f72afaa8ca9ddfbf41
awils18 <awils18@u.rochester.edu>
2013-12-03 18:57:00 -0500

    New Pages

--- URFitProject/URFit/src/main/java/com/URFitness/MainActivity.java
+++ URFitProject/URFit/src/main/java/com/URFitness/MainActivity.java
@@ -1,5 +1,8 @@
 package com.URFitness;
 
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
@@ -17,16 +20,26 @@ import android.view.ViewGroup;
 import android.view.View.OnClickListener;
 import android.os.Build;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.BasicResponseHandler;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
 
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -41,57 +54,88 @@ public class MainActivity extends ActionBarActivity {
         }
     }
     public void jumpToPage(View v){
+        //httppost= new HttpPost("\"http://urfitness.org/mobile_login.php?username=\"+uname+\"&password=\"+upass"); // make sure the url is correct.
+
+        Button b;
+        EditText et,pass;
+        final TextView tv;
+        HttpPost httppost;
+        StringBuffer buffer;
+        //HttpResponse response;
+        HttpClient httpclient;
+        List<NameValuePair> nameValuePairs;
+        final ProgressDialog dialog = null;
+
         String result = null;
 
         EditText mEditu = (EditText) findViewById(R.id.userLogin);
-        System.out.println("1");
         EditText mEditp = (EditText) findViewById(R.id.editText2);
-        System.out.println("2");
 
         String uname= mEditu.getText().toString();
         String upass=mEditp.getText().toString();
-        System.out.println("3");
-        HttpClient client = new DefaultHttpClient();
-        System.out.println("4");
 
-         String tem="http://urfitness.org/mobile_login.php?username="+uname+"&password="+upass;
-        HttpGet request = new HttpGet(tem);
-        try
-        {
-            HttpResponse response=client.execute(request);
-            System.out.println("5");
-            HttpEntity entity=response.getEntity();
-            System.out.println("6");
-            InputStream is=entity.getContent();
-            System.out.println("7");
-            InputStreamReader isr = new InputStreamReader(is);
-            System.out.println("8");
-            BufferedReader reader = new BufferedReader(isr);
-            System.out.println("9");
-            result=reader.readLine();
-            System.out.println("10");
+        System.out.println(uname + " |||||||||||||||||||" + upass);
+
+        //httpclient=new DefaultHttpClient();
+        //httppost= new HttpPost("http://urfitness.org/mobile_login.php?username=daniel.weiner@rochester.edu&password=poop");
+
+        try{
+
+            httpclient=new DefaultHttpClient();
+            httppost= new HttpPost("http://urfitness.org/mobile_login.php?username=daniel.weiner@rochester.edu&password=poop");
+            System.out.print("HEEYEYYEYEYEYEYEYEYEEYEYYEEYEY");
+            nameValuePairs = new ArrayList<NameValuePair>(2);
+            // Always use the same variable name for posting i.e the android side variable name and php side variable name should be similar,
+            nameValuePairs.add(new BasicNameValuePair("username",mEditu.getText().toString().trim()));  // $Edittext_value = $_POST['Edittext_value'];
+            nameValuePairs.add(new BasicNameValuePair("password",mEditp.getText().toString().trim()));
+            httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+            //Execute HTTP Post Request
+            //response =httpclient.execute(httppost);
+            ResponseHandler<String> responseHandler = new BasicResponseHandler();
+            final String response = httpclient.execute(httppost, responseHandler);
+            System.out.println("Response : " + response);
+            runOnUiThread(new Runnable() {
+                public void run() {
+                    //tv.setText("Response from PHP : " + response);
+                    dialog.dismiss();
+                }
+            });
+
+            if(response.equalsIgnoreCase("User Found")){
+                runOnUiThread(new Runnable() {
+                    public void run() {
+                        Toast.makeText(MainActivity.this,"Login Success", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+                startActivity(new Intent(MainActivity.this, HomeActivity.class));
+            }else{
+                showAlert();
+            }
+
+        }catch(Exception e){
+            dialog.dismiss();
+            System.out.println("Exception : " + e.getMessage());
         }
-
-        catch (Exception e)
-        {
-            e.printStackTrace();
-        }
-
-        if(result.equals("1")==true)
-        {
-            Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
-            startActivity(intent);
-
-        }
-        else
-        {
-
-
-        }
-
-
-
     }
+    public void showAlert(){
+        this.runOnUiThread(new Runnable() {
+            public void run() {
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle("Login Error.");
+                builder.setMessage("User not Found.")
+                        .setCancelable(false)
+                        .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                            }
+                        });
+                AlertDialog alert = builder.create();
+                alert.show();
+            }
+        });
+    }
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
