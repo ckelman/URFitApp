7b78d3ffa2e87bc75830c110777692ef99cbc424
ckelman <cikelman@u.rochester.edu>
2013-12-04 22:17:34 -0500

    changes

--- URFitProject/URFit/src/main/java/com/URFitness/MainActivity.java
+++ URFitProject/URFit/src/main/java/com/URFitness/MainActivity.java
@@ -73,6 +73,8 @@ public class MainActivity extends ActionBarActivity {
 
             StrictMode.setThreadPolicy(policy);
         }
+
+
     }
     public void jumpToPage(View v) throws IOException{
 
@@ -86,126 +88,59 @@ public class MainActivity extends ActionBarActivity {
 
         System.out.println(uname + " |||||||||||||||||||" + upass);
 
-       // httpclient=new DefaultHttpClient();
-        //httppost= new HttpPost("http://urfitness.org/mobile_login.php?username=daniel.weiner@rochester.edu&password=poop");
+        String url = "http://urfitness.org/mobile_login.php?username="+uname+"&password="+upass;
 
-
-
-/**
-        String result = "";
-        InputStream is = null;
-        ArrayList<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
-        nameValuePairs.add(new BasicNameValuePair("username","awils18@u.rochester.edu"));
-        nameValuePairs.add(new BasicNameValuePair("password","alexwilson"));
-
-        try{
-            HttpClient httpclient = new DefaultHttpClient();
-            HttpPost httppost = new HttpPost("http://urfitness.org/mobile_login.php?username=daniel.weiner@rochester.edu&password=poop");
-            System.out.println("WELL THIS WORKS");
-            //httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
-            HttpResponse response = httpclient.execute(httppost);
-            HttpEntity entity = response.getEntity();
-            is = entity.getContent();
-        }catch(Exception e){
-            Log.e("log_tag", "Error in http connection "+e.toString());
-        }
-
-        try{
-            BufferedReader reader = new BufferedReader(new InputStreamReader(is,"iso-8859-1"),8);
-            StringBuilder sb = new StringBuilder();
-            String line = null;
-            while ((line = reader.readLine()) != null) {
-                sb.append(line + "\n");
-            }
-            is.close();
-
-            result=sb.toString();
-        }catch(Exception e){
-            Log.e("log_tag", "Error converting result "+e.toString());
-        }
-
-//parse json data
-        try{
-            JSONArray jArray = new JSONArray(result);
-            for(int i=0;i<jArray.length();i++){
-                JSONObject json_data = jArray.getJSONObject(i);
-                Log.i("log_tag","id: "+json_data.getInt("id")+
-                        ", name: "+json_data.getString("name")+
-                        ", sex: "+json_data.getInt("sex")+
-                        ", birthyear: "+json_data.getInt("birthyear")
-                );
-            }
-        }
-    catch(JSONException e){
-        Log.e("log_tag", "Error parsing data "+e.toString());
-    }**/
     //System.out.println(inputStreamToString(getInputStreamFromUrl("http://urfitness.org/mobile_login.php?username=daniel.weiner@rochester.edu&password=poop")));
+        try
+        {
+            String ans  = sendGet(url);
 
-       int ans  = connect("http://urfitness.org/mobile_login.php?username=daniel.weiner@rochester.edu&password=poop");
-        //System.out.println("*************"+ans);
-        if (ans == -1)
-        {
-        Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
-        startActivity(intent);
+            if(ans.equals("1"))
+            {
+                Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
+                startActivity(intent);
+            }
+            else
+            {
+                Intent intent = new Intent(getApplicationContext(), LoginFailActivity.class);
+                startActivity(intent);
+            }
+            System.out.println(ans);
         }
-        else if (ans == 0)
+        catch(Exception e)
         {
-            Intent intent = new Intent(getApplicationContext(), WeightOptionsActivity.class);
-            startActivity(intent);
+            System.out.println(e);
         }
-        else
-        {
-            Intent intent = new Intent(getApplicationContext(), SportsOptionsActivity.class);
-            startActivity(intent);
-        }
+
+
+
+
     }
 
-    public static int connect(String url)
-    {
-
-        //System.out.println("1");
-        HttpClient httpclient = new DefaultHttpClient();
-        //System.out.println("2");
-        // Prepare a request object
-        HttpGet httpget = new HttpGet(url);
-        //System.out.println("3");
-
-        // Execute the request
-        HttpResponse response;
-        try {
-            response = httpclient.execute(httpget);
-            //System.out.println("4");
-            // Examine the response status
-            Log.i("Praeda",response.getStatusLine().toString());
-            //System.out.println("5");
+    private String sendGet(String url) throws Exception {
 
 
-            // Get hold of the response entity
-            HttpEntity entity = response.getEntity();
-           // System.out.println("6");
-            // If the response does not enclose an entity, there is no need
-            // to worry about connection release
+        HttpClient client = new DefaultHttpClient();
+        HttpGet request = new HttpGet(url);
 
 
-            if (entity != null) {
+        HttpResponse response = client.execute(request);
 
-                // A Simple JSON Response Read
-                InputStream instream = entity.getContent();
-                String result= convertStreamToString(instream);
-                // now you have the string representation of the HTML request
-                System.out.print("HERE WE ARE" + result);
-                instream.close();
+        System.out.println("\nSending 'GET' request to URL : " + url);
+        System.out.println("Response Code : " +
+                response.getStatusLine().getStatusCode());
 
-                if(result.equals("1"))
-                    return 1;
-                else
-                    return -1;
-            }
+        BufferedReader rd = new BufferedReader(
+                new InputStreamReader(response.getEntity().getContent()));
 
+        StringBuffer result = new StringBuffer();
+        String line = "";
+        while ((line = rd.readLine()) != null) {
+            result.append(line);
+        }
 
-        } catch (Exception e) { System.out.print("THERE IS A CONNECTION ERROR\n"+e);}
-
-        return 0;
+        System.out.println(result.toString());
+        return result.toString();
 
     }
 
