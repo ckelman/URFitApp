39ab45970007021df478abb69bf5a1d4e51ed849
awils18 <awils18@u.rochester.edu>
2013-12-09 21:56:28 -0500

    New Pages

--- URFitProject/URFit/src/main/java/com/URFitness/MainLoading.java
+++ URFitProject/URFit/src/main/java/com/URFitness/MainLoading.java
@@ -40,7 +40,7 @@ public class MainLoading extends ActionBarActivity {
             public void run(){
                 try{
                     int logoTimer = 0;
-                    while(logoTimer < 5000){
+                    while(logoTimer < 10000){
                         sleep(100);
                         logoTimer = logoTimer +100;
                     };
